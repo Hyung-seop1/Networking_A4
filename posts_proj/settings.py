005d963dda621ab1c3b389dd00fe155dd5ee1595
Hyungseop Lee <hlee8291@conestogac.on.ca>
2025-04-01 17:45:57 -0400

    creating base.html

--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -120,6 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = {
+    BASE_DIR / 'static',
+    BASE_DIR / 'posts' / 'static',
+    BASE_DIR / 'profiles' / 'static',
+}
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
