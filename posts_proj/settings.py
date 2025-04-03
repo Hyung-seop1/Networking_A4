3ce18dc3a878b96d5794f48c1b4ee0a9b0e1c204
Hyungseop Lee <hlee8291@conestogac.on.ca>
2025-04-02 21:38:22 -0400

    successfully adding form to the Modal

--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'profiles',
     # 3rd party apps
     'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -133,3 +135,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
